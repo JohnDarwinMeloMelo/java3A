a9275814aa9223cb480f775d1162c67d20708cc6
JohnDarwinMeloMelo <jhon.darwin.melo105@gmail.com>
2020-03-05 00:41:44 -0500

    processing

--- CARRERA NUMERICA/src/carrera/numerica/CARRERANUMERICA.java
+++ CARRERA NUMERICA/src/carrera/numerica/CARRERANUMERICA.java
@@ -22,7 +22,10 @@ public class CARRERANUMERICA {
      Random aleatorio = new Random();
      Scanner Datos;
      Datos = new Scanner(System.in);
-     int numj,opc=1,ganador=0;
+     int vector=0;
+     int vectores[]= new int[10];
+     int b=0,a=1,numj=0,opc=1,ganador=0,i=0,res1,contPar=0;
+     
      System.out.println("          CARRERA NUMERICA");
      System.out.println("   *Numeros de jugadores minimo 2");
      System.out.println("   *Numeros de jugadores maximo 4");
@@ -40,19 +43,50 @@ public class CARRERANUMERICA {
      System.out.println("2. NIVEL INTERMEDIO (TABLERO DE 30 POSICIONES)");
      System.out.println("3. NIVEL AVANZADO (TABLERO DE 50 POSICIONES)");
      opc = Datos.nextInt();
+     String entrada = Datos.nextLine();
+     System.out.println(entrada);
      switch (opc){
         case 1:
             while (ganador<=20){
-            System.out.println("----JUGADOR NUMERO 1");
-            System.out.println("presione enter para lanzar los dados");
-            String entrada = Datos.nextLine();
-            System.out.println(entrada);
-            int dado1 = 1 + aleatorio.nextInt(6);
-            int dado2 = 1 + aleatorio.nextInt(6);
-            System.out.println("-DADO 1:  "+dado1);
-            System.out.println("-DADO 2:  "+dado2);
-            }//cierre de while 20
-            
+                b=0;
+                a=1;
+                System.out.println("------------------------------");    
+                System.out.println("------------------------------");
+                while (a<=numj){
+                    
+                    System.out.println("pocicion de jugador ("+a+")= "+vectores[a]);
+                    a++;
+                    
+                }
+                    
+                    
+                    
+                    
+                    
+                for (i=1;i<=numj;i++){
+                    System.out.println("------------------------------");    
+                    System.out.println("------------------------------");
+                    System.out.println("----JUGADOR NUMERO "+i);
+                    System.out.println("presione enter para lanzar los dados");
+                    String entra = Datos.nextLine();
+                    System.out.println(entra);
+                    int dado1 = 1 + aleatorio.nextInt(6);
+                    int dado2 = 1 + aleatorio.nextInt(6);
+                    System.out.println("-DADO 1:  "+dado1);
+                    System.out.println("-DADO 2:  "+dado2);
+                    if (dado1==dado2){
+                        contPar++;
+                    }else{
+                        contPar=0;
+                    } //cieree de if dado igual
+                    if (contPar==3){
+                        ganador=20;
+                    }//cierre de contador ganador 
+                    vector=dado1+dado2;
+                    vectores[i]=vector+vectores[i];
+                    
+            }//cierre for
+            }//cierre de while 
             
             
         break; //cierre case 1
