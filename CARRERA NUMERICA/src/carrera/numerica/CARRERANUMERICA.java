ead7384f909d07d2d3555fa4e1bb71ee7904ea4a
JohnDarwinMeloMelo <jhon.darwin.melo105@gmail.com>
2020-03-05 23:25:09 -0500

    processing

--- CARRERA NUMERICA/src/carrera/numerica/CARRERANUMERICA.java
+++ CARRERA NUMERICA/src/carrera/numerica/CARRERANUMERICA.java
@@ -24,7 +24,7 @@ public class CARRERANUMERICA {
      Datos = new Scanner(System.in);
      int vector=0;
      int vectores[]= new int[10];
-     int b=0,a=1,numj=0,opc=1,ganador=0,i=0,res1,contPar=0;
+     int b=0,a=1,numj=0,opc=1,ganador=0,i=0,res1,contPar=0,gana=0;
      
      System.out.println("          CARRERA NUMERICA");
      System.out.println("   *Numeros de jugadores minimo 2");
@@ -33,7 +33,8 @@ public class CARRERANUMERICA {
      numj = Datos.nextInt();
      if (numj<2 || numj>4 ){
      while (numj<2 || numj>4 ){
-        System.out.println("-----ERROR JUGADORES MINIMO 2 Y MAXIMO 4------");
+         
+         System.out.println("-----ERROR JUGADORES MINIMO 2 Y MAXIMO 4------");
          System.out.println("--Digite numeros de jugadores: ");
         numj = Datos.nextInt();
      }// cierre de while jugadores
@@ -47,11 +48,12 @@ public class CARRERANUMERICA {
      System.out.println(entrada);
      switch (opc){
         case 1:
-            while (ganador<=20){
+            while (ganador<20){
                 b=0;
                 a=1;
                 System.out.println("------------------------------");    
                 System.out.println("------------------------------");
+                System.out.println("\n\n\n\n\n");
                 while (a<=numj){
                     
                     System.out.println("pocicion de jugador ("+a+")= "+vectores[a]);
@@ -84,9 +86,18 @@ public class CARRERANUMERICA {
                     }//cierre de contador ganador 
                     vector=dado1+dado2;
                     vectores[i]=vector+vectores[i];
-                    
+                    if (vectores[i]>=20){
+                    ganador=20;
+                    gana=i;
+                    i=numj+1;
+                    }
             }//cierre for
             }//cierre de while 
+            if (ganador==20){
+                System.out.println("¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡");
+                System.out.println("¡¡¡   FIN DEL JUEGO  ¡¡¡¡");
+                System.out.println("GANADOR JUGADOR => "+gana);
+            }    
             
             
         break; //cierre case 1
